32706dd93f287fdce362f1119bf1d7c2cf4ef6f6
Houssna BOUTAJAR <houssna.boutajar@edu.univ-paris13.fr>
2024-01-19 00:21:22 +0000

    Upload New File

---
+++ Autoformation/Houssna Boutajar/Views/view_home.php
@@ -0,0 +1,5 @@
+<?php require_once "view_begin.php" ?>
+<h1> Les meilleurs Disney </h1>
+<p> Bienvenue sur notre site des meilleurs Disney ?></strong> . Vous pouvez modifier cette liste en ajoutant un film,en effacant un film,en modifiant les informations d'un film. Vous pouvez aussi lister tout les films differents. </p>
+
+<?php require "view_end.php"; ?>
