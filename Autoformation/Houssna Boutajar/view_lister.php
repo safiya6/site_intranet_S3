f391db8070fc8a42c6931c57a35b3a7dd7edb0ca
Houssna BOUTAJAR <houssna.boutajar@edu.univ-paris13.fr>
2024-01-19 00:22:30 +0000

    Upload New File

---
+++ Autoformation/Houssna Boutajar/view_lister.php
@@ -0,0 +1,14 @@
+<?php require "view_begin.php";?>
+<h1> Lister les films</h1>
+<ul>
+<form action="traitementFormulaire.php" method="post">
+    <p> Titre : <input name="Titre" type="text"/> <br/>
+    Anneesortie: <input name="AnneeSortie" type="text"/> <br/>
+    Duree : <input name="duree" type="text"/> <br/>
+    Realisateur : <input name="titre" type="text"/> <br/>
+    Id : <input name="id" type="text"/> <br/>
+</ul>
+</main>
+</body>
+</html>
+<?php require "view_end.php" ?>
