0a5f37884080582bb29b66e85077766da933bac5
Houssna BOUTAJAR <houssna.boutajar@edu.univ-paris13.fr>
2024-01-19 00:20:58 +0000

    autoformation houssna

---
+++ Autoformation/Houssna Boutajar/Views/view_begin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>Les meilleurs Disney</title>
+    <link rel="stylesheet" href="utils/style.css">
+
+</head>
+<body>
+    <header>
+        <h1>Formulaire Disney Autoformation de Houssna Boutajar</h1>
+    </header>
+
+    <nav>
+        <form action="" method="get">
+            <button type="submit" formaction="?controller=list&action=modifier">Modifier un film</button>
+            <button type="submit" formaction="?controller=list&action=listerFilms">Lister les films</button>
+            <button type="submit" formaction="?controller=list&action=ajouter">Ajouter un film</button>
+            <button type="submit" formaction="?controller=list&action=effacer">Effacer un film</button>
+        </form>
+    </nav>
+
+    <main>
+        <p>Bienvenue sur notre site des meilleurs Disney. Vous pouvez modifier cette liste en ajoutant un film, en effaçant un film, en modifiant les informations d'un film. Vous pouvez aussi lister tous les films différents.</p>
+    </main>
+</body>
+</html>
