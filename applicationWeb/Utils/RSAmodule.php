6785846b7b1ff508836d019f6f5dac08f64bf818
Tom JEDOROWICZ <tom.jedorowicz@edu.univ-paris13.fr>
2024-01-19 15:20:41 +0000

    Update applicationWeb/Utils/RSAmodule.php

--- applicationWeb/Utils/RSAmodule.php
+++ applicationWeb/Utils/RSAmodule.php
@@ -1,49 +1,56 @@
 <?php
-
-
 require 'keys.php';
+
 $privKey = PRIVATE_KEY;
 $pubKey = PUBLIC_KEY;
 
+// Fonction qui vérifie si les mots de passe de la base de données sont chiffrés
+function verifMdp($tab)
+{
+    $nb344 = 0;
+    $nbTot = 0;
 
-//verifie si bdd chiffré ou pass
-function verifMdp($tab){
-    foreach($tab as $row){
-		$id = $row[0];
-		$mdp = $row[1];
-		$nb344=0;
-        $nbTot=0;
-        if(strlen($mdp) == 344){
+    foreach ($tab as $row) {
+        $id = $row[0];
+        $mdp = $row[1];
+
+        // Vérification de la longueur du mdp
+        if (strlen($mdp) == 344) {
             $nb344++;
             $nbTot++;
-        }
-        else{
+        } else {
             $nbTot++;
         }
     }
-    if($nb344==$nbTot){
+
+    // Si tous les mots de passe sont de longueur 344, la base de données est chiffrée
+    if ($nb344 == $nbTot) {
         return strlen($mdp);
-    }
-    else{
+    } else {
         return 0;
     }
-} 
+}
 
+// chiffrement RSA
+function encryptRSA($data, $publicKey)
+{
+    // Chiffrement  avec la clé publique
+    openssl_public_encrypt($data, $encrypted, $publicKey);
 
-function encryptRSA($data,$publicKey){
-    openssl_public_encrypt($data,$encrypted,$publicKey);
+    
     return base64_encode($encrypted);
 }
 
-function decryptRSA($data, $privateKey) {
+// fonction de déchiffrement 
+function decryptRSA($data, $privateKey)
+{
+    // Déchiffrement de la donnée en base64 avec la clé privée
     if (!openssl_private_decrypt(base64_decode($data), $decrypted, $privateKey)) {
         die("Erreur lors du déchiffrement : " . openssl_error_string());
     }
+
+   
     return $decrypted;
-    
-    echo "dechiffrement";
+    // Note : la ligne 'echo "dechiffrement";' ne sera jamais exécutée car elle est suivie par un 'return'
 }
-
-
-
-
+?>
