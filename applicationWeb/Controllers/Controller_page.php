81a07bf5179656c4bda270dbbf240bfa0438cfd2
Tom JEDOROWICZ <tom.jedorowicz@edu.univ-paris13.fr>
2024-01-19 15:01:45 +0000

    Update applicationWeb/Controllers/Controller_page.php

--- applicationWeb/Controllers/Controller_page.php
+++ applicationWeb/Controllers/Controller_page.php
@@ -1,144 +1,127 @@
 <?php
- session_start();
+// Démarre la session 
+session_start();
 
 class Controller_page extends Controller
 {
+    // Action par défaut qui affiche la page de test
     public function action_default() {
         $this->render("test");
     }
+
+    // Action pour récupérer les informations d'un utilisateur
     public function action_utilisateur(){
-
         $m = Model::getModel();
-        $SC= $m->getServiceStatutairevsComplémentaire($_SESSION["semestre"]);
-        $_SESSION["S_vs_C"]=json_encode($SC);
-        
-
+        // Récupère le service statutaire vs complémentaire pour le semestre en cours
+        $SC = $m->getServiceStatutairevsComplémentaire($_SESSION["semestre"]);
+        $_SESSION["S_vs_C"] = json_encode($SC);
     } 
     
+    // Action pour un enseignant
     public function action_enseignant()
     {
         $m = Model::getModel();
-        $list = $m->getQuotite(((int)$_SESSION["ide"]));
-        $_SESSION["heureens"] = json_encode($m->gethorairestravail($_SESSION["ide"] ,$_SESSION["semestre"]));
+        // Récupère la quotité de l'enseignant
+        $list = $m->getQuotite((int)$_SESSION["ide"]);
+        $_SESSION["heureens"] = json_encode($m->gethorairestravail($_SESSION["ide"], $_SESSION["semestre"]));
         $monTableauJSON = json_encode($list);   
         $_SESSION["quotite"]  = $monTableauJSON ;
-        $this-> render("page_identifiant");
+        $this->render("page_identifiant");
     }
 
+    // Action pour un secrétaire
     public function action_secretaire()
     {
         $m = Model::getModel();
-        $_SESSION["departement"]= $m->get_departementSecretaire($_SESSION["ide"]);
+        $_SESSION["departement"] = $m->get_departementSecretaire($_SESSION["ide"]);
         $this->action_utilisateur();
         $_SESSION["enseigneDept"] = json_encode($m->getEnseignantparDept($_SESSION["departement"]));
-        $_SESSION["HeurediscIUTDept"] = json_encode($m->getHeurediscIUTDept($_SESSION["departement"],$_SESSION["semestre"],$_SESSION["niveau"]));
-        $this-> render("page_identifiant");
+        $_SESSION["HeurediscIUTDept"] = json_encode($m->getHeurediscIUTDept($_SESSION["departement"], $_SESSION["semestre"], $_SESSION["niveau"]));
+        $this->render("page_identifiant");
     }
 
+    // Action pour un chef de département
     public function action_chefDept()
     {
-        
         $this->action_utilisateur();
         $m = Model::getModel();
         $_SESSION["departement"] = $m->recup_departement($_SESSION["ide"]);
         $_SESSION["enseigneDept"] = json_encode($m->getEnseignantparDept($_SESSION["departement"]));
-        $_SESSION["HeurediscIUTDept"] = json_encode($m->getHeurediscIUTDept($_SESSION["departement"],$_SESSION["semestre"],$_SESSION["niveau"]));
+        $_SESSION["HeurediscIUTDept"] = json_encode($m->getHeurediscIUTDept($_SESSION["departement"], $_SESSION["semestre"], $_SESSION["niveau"]));
         $this->action_enseignant(); 
-        $this-> render("page_identifiant");
-     
-       
+        $this->render("page_identifiant");
     }
 
-
+    // Action pour un directeur
     public function action_directeur()
-    {   $this-> action_utilisateur();
+    {   
+        $this->action_utilisateur();
         $m=Model::getModel();
         $_SESSION["pourcentage"]= json_encode($m->getPourcentageAgent() );
-        $_SESSION["HeurediscIUT"] = json_encode($m->getHeurediscIUT($_SESSION["semestre"],$_SESSION["niveau"] ));
+        $_SESSION["HeurediscIUT"] = json_encode($m->getHeurediscIUT($_SESSION["semestre"], $_SESSION["niveau"] ));
         $this->action_enseignant();
-        $this-> render("page_identifiant");
-
+        $this->render("page_identifiant");
     }
 
-   public function action_recupSemestre(){
-       
-        if (!isset($_POST["semestre"] )&!isset($_POST["niveau"])){
+    // Action pour récupérer le semestre sélectionné
+    public function action_recupSemestre(){
+        if (!isset($_POST["semestre"]) && !isset($_POST["niveau"])){
             $_SESSION["semestre"] = 1;
-        }else{
+        } else {
            $_SESSION["semestre"] = $_POST["semestre"] ;
         } 
-        if($_SESSION["role"]== "directeur") {
-            $this-> action_directeur();
-        }
-        elseif ($_SESSION["role"] == "chef de departement"){
-            $this-> action_chefDept();
-        }elseif($_SESSION["role"]=="secretaire"){
-            $this-> action_secretaire();
-        } 
-        elseif($_SESSION["role"] == "enseignant"){
-            $this-> action_enseigant();
-        }
-        else {
-            $this-> action_directeur();
+        // Redirection en fonction du rôle de l'utilisateur
+        if ($_SESSION["role"] == "directeur") {
+            $this->action_directeur();
+        } elseif ($_SESSION["role"] == "chef de departement") {
+            $this->action_chefDept();
+        } elseif ($_SESSION["role"] == "secretaire") {
+            $this->action_secretaire();
+        } elseif ($_SESSION["role"] == "enseignant") {
+            $this->action_enseignant();
+        } else {
+            $this->action_directeur();
         } 
     } 
+
+    // Action pour récupérer le niveau sélectionné
     public function action_recupNiveau(){
-       
         if (!isset($_POST["niveau"])){
             $_SESSION["niveau"] = 1;
-            
-        }else{
-           $_SESSION["niveau"]=$_POST["niveau"];
-           
+        } else {
+           $_SESSION["niveau"] = $_POST["niveau"];
         } 
-        if($_SESSION["role"]== "directeur") {
-            $this-> action_directeur();
-        }
-        elseif ($_SESSION["role"] == "chef de departement"){
-            $this-> action_chefDept();
-        }elseif($_SESSION["role"]=="secretaire"){
-            $this-> action_secretaire();
-        } 
-        elseif($_SESSION["role"] == "enseignant"){
-            $this-> action_enseigant();
-        }
-        else {
-            $this-> action_directeur();
+        // Redirection en fonction du rôle de l'utilisateur
+        if ($_SESSION["role"] == "directeur") {
+            $this->action_directeur();
+        } elseif ($_SESSION["role"] == "chef de departement") {
+            $this->action_chefDept();
+        } elseif ($_SESSION["role"] == "secretaire") {
+            $this->action_secretaire();
+        } elseif ($_SESSION["role"] == "enseignant") {
+            $this->action_enseignant();
+        } else {
+            $this->action_directeur();
         } 
     } 
-   /*public function action_rendre(){
-        
-        if ($_SESSION["role"]== "secrétaire"){
+
+    // Commenté pour l'instant
+    /*public function action_rendre(){
+        if ($_SESSION["role"] == "secrétaire"){
             $this->action_secretaire();
             $this->render("secretaire");
-        } 
-        elseif ($_SESSION["role"] == "enseignant"){
-            $this-> action_enseignant();
+        } elseif ($_SESSION["role"] == "enseignant"){
+            $this->action_enseignant();
             $this->render('enseignant');
-        } 
-        elseif ($_SESSION["role"]== "directeur" ){
-            $this-> action_directeur();
+        } elseif ($_SESSION["role"] == "directeur" ){
+            $this->action_directeur();
             $this->render("directeur");
+        } elseif ($_SESSION["role"] == "chef de departement"){
+            $this->action_chefDept();
+            $this->render("chefdept");
         } 
-        elseif ($_SESSION["role"]=="chef de departement"){
-            $this-> action_chefDept();
-            $this -> render("chefdept");
-        } 
-   } */
+    } */
 
     /**public function action_donneEnseignant() {
         session_start();
-        $m = Model::getModel();
-        if ($m->est_enseignant($_SESSION["ide"])){
-            $list = $m->getQuotite(((int)$_SESSION["ide"]));
-    
-            $monTableauJSON = json_encode($list);
-           
-            $_SESSION["quotite"]  = $monTableauJSON ;
-        }  
-          
-        $this->render("page_identifiant");
-  
-    } */
-}
-?>
+        $m = Model::getModel
